2d40ad988957e1070946b60c2e27da635e168586
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-11 09:02:38 +0600

    Initialize the express application

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { sendSuccessResponse } from './helper.js';
+import plyrRoutes from './routes/plyr.routes.js';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  sendSuccessResponse(res, 'Success');
+});
+
+app.use('/plyr', plyrRoutes);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({
+    message: err.message,
+    hint: err.hint ?? null,
+  });
+});
+
+export default app;
