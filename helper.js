7fa40d70c8e55dedf8ac2bb6d1e9113dd8543537
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-11 09:02:58 +0600

    Create helper function

---
+++ helper.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {import("express").Response} res The response object
+ * @param {string} message The message
+ * @param {object | null} result
+ */
+export const sendSuccessResponse = (res, message, result) => {
+  return res.status(200).json({
+    status: res.statusCode,
+    message,
+    result,
+  });
+};
