559bfae6847a123e68d9a1b1d89d6e5561862c2f
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-11 11:35:19 +0600

    Update plyr.routes.js

--- routes/plyr.routes.js
+++ routes/plyr.routes.js
@@ -25,7 +25,10 @@ router.get('/iframe', (req, res) => {
     const htmlContent = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Childe Anime Player</title><link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" /><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"><style>:root {--plyr-color-main: rgb(0, 112, 112);} body {font-family: 'Inter', sans-serif;} button.plyr__control--overlaid {display:none; visibility: hidden;} </style></head><body><video controls crossorigin playsinline id="player">${sourcesHTML}${tracksHTML}</video><script src="https://cdn.plyr.io/3.7.8/plyr.js"></script><script>document.addEventListener('DOMContentLoaded', () => {const selector = '#player';const player = new Plyr(selector, { controls: ["play", "progress", "current-time", "duration", "mute", "volume", "settings", "pip", "airplay", "fullscreen"], i18n: { play: "Play (K)", pause: "Pause (K)", mute: "Mute (M)", unmute: "Unmute (M)", enterFullscreen: "Enter fullscreen (F)", exitFullscreen: "Exit fullscreen (F)", qualityLabel: { 0: "Auto" } }, seekTime: 5, keyboard: { focused: !0, global: !0 }, fullscreen: { iosNative: !0 } }); window.player = player; });</script></body></html>`;
 
     // Encode the HTML content
-    const encodedHtmlContent = encodeURIComponent(htmlContent);
+    const encodedHtmlContent = encodeURIComponent(htmlContent).replace(
+      /"/g,
+      '\\"'
+    );
 
     // Set the encoded HTML content as the srcdoc attribute
     const iframeContent = `<iframe srcdoc="${encodedHtmlContent}" width="640" height="360" frameborder="0" allowfullscreen></iframe>`;
