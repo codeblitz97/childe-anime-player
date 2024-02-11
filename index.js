b59530fe3d6341f6b9e8a9864c2eae05e5f644cc
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-11 09:16:00 +0600

    Remove chalk cause I don't know

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const chalk = require('chalk');
 const app = require('./app');
 
 let PORT = process?.env?.PORT ?? 3002;
 
 app.listen(PORT, () => {
-  console.log(chalk.greenBright('Success'), `Server is listening on`, PORT);
+  console.log(`Server is listening on`, PORT);
 });
